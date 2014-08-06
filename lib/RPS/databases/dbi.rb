1899c4265a218ad5f0a17e5416a8d98f2143f3fa
Allison Zadrozny <allison@zadrozny.com>
2014-08-06 09:47:29 -0500

    Added build tables method to dbi.rb

--- lib/RPS/databases/dbi.rb
+++ lib/RPS/databases/dbi.rb
@@ -11,7 +11,25 @@ module RPS
     def build_tables
       @db.exec(%q[
         CREATE TABLE IF NOT EXISTS users(
-          id serial NOT NULL PRIMARY KEY,
+          user_id serial NOT NULL PRIMARY KEY,
+          username text,
+          password_digest text,
+          created_at timestamp NOT NULL DEFAULT current_timestamp
+          )])
+      @db.exec(%q[
+        CREATE TABLE IF NOT EXISTS games(
+          game_id serial NOT NULL PRIMARY KEY,
+          p1_id integer,
+          p2_id integer,
+          game_winner integer
+          )])
+      @db.exec(%q[
+        CREATE TABLE IF NOT EXISTS rounds(
+          round_id serial NOT NULL PRIMARY KEY,
+          game_id integer,
+          p1_move text,
+          p2_move text,
+          round_winner integer
           )])
       # USERS
       # GAMES
